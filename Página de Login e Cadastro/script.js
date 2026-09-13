be9ccffde9f4b3747f15a0750d7b10d6ab1ebc37
Rem2v <00001092975263sp@al.educacao.sp.gov.br>
2026-09-13 19:52:18 -0300

    Reorganização de pastas

---
+++ Página de Login e Cadastro/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Simulate a delay before redirecting
+    setTimeout(function() {
+        window.location.href = 'Página de Cadastro/index.html';
+    }, 5000); // 5 seconds delay
+});
