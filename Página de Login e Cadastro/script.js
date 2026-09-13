15a34b5a0e2b660ed9ed9512a5d0ba58f59bfc62
Rem2v <00001092975263sp@al.educacao.sp.gov.br>
2026-09-13 20:06:39 -0300

    Revert "Separação por pasta"
    This reverts commit fc481420205ce38e8da4611b07a9e4312fce4833.

--- Página de Login e Cadastro/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Simulate a delay before redirecting
-    setTimeout(function() {
-        window.location.href = 'Página de Cadastro/index.html';
-    }, 5000); // 5 seconds delay
-});
